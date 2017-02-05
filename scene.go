878f9ffd2a554e6b218f87453296c09c5f4a1398
Rubén Simón <ruben.simon@socialpoint.es>
2017-02-05 11:26:11 +0100

    Add pipes with collision detection

--- scene.go
+++ scene.go
@@ -8,11 +8,16 @@ import (
 	img "github.com/veandco/go-sdl2/sdl_image"
 )
 
+const (
+	minPipeDist = 300
+)
+
 type scene struct {
 	renderer *sdl.Renderer
 	bg       *sdl.Texture
 	bgt      int32
 	bird     *bird
+	pipes    *pipes
 }
 
 func newScene(r *sdl.Renderer, speed int32, gravity float64) (s *scene, err error) {
@@ -32,20 +37,43 @@ func newScene(r *sdl.Renderer, speed int32, gravity float64) (s *scene, err erro
 	}
 	for i := 1; i <= 4; i++ {
 		path := fmt.Sprintf("res/imgs/bird_frame_%d.png", i)
-		frame, err := img.LoadTexture(r, path)
-		if err != nil {
+		frame, errLoad := img.LoadTexture(r, path)
+		if errLoad != nil {
 			return nil, fmt.Errorf("could not load bird_frame_%d image: %v", i, err)
 		}
 		s.bird.frames = append(s.bird.frames, frame)
 	}
 
+	s.pipes = &pipes{
+		speed: speed,
+		pipes: initialPipes(),
+	}
+
+	s.pipes.texture, err = img.LoadTexture(r, "res/imgs/pipe.png")
+	if err != nil {
+		return nil, fmt.Errorf("could not load pipe texture: %v", err)
+
+	}
+
 	return s, nil
+}
+
+func initialPipes() []*pipe {
+	x := int32(windowWidth)
+	pp := make([]*pipe, 0)
+	for i := 0; i < 4; i++ {
+		pp = append(pp, newPipe(x))
+		x = x + minPipeDist
+	}
+
+	return pp
 }
 
 func (s *scene) restart() {
 	s.bird.y = windowHeight / 2
 	s.bird.dead = false
 	s.bird.speed = 0
+	s.pipes.pipes = initialPipes()
 }
 
 func (s *scene) run(ctx context.Context, fps float64) {
@@ -66,7 +94,12 @@ func (s *scene) run(ctx context.Context, fps float64) {
 
 func (s *scene) update() {
 	s.bird.update()
+	s.pipes.update()
 	s.bgt = (s.bgt + 1) % 2000
+
+	if s.pipes.hits(s.bird) {
+		s.bird.dead = true
+	}
 }
 
 func (s *scene) draw() error {
@@ -79,6 +112,7 @@ func (s *scene) draw() error {
 	}
 
 	s.bird.draw(s.renderer)
+	s.pipes.draw(s.renderer)
 
 	if s.bird.dead {
 		deadRect := &sdl.Rect{X: 100, Y: windowHeight / 4, W: windowWidth - 200, H: windowHeight / 2}
